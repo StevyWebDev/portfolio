f13c9ce828daf04e9f3fef3abad8ad5241b45db0
StevyWebDev <StevyWebDev@gmail.com>
2021-12-21 17:55:49 +0100

    Header Ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Header from './components/Header';
+import Header from "./components/Header";
+import Home from "./components/Home";
+//import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <main>
+            <Home />
+        </main>
     </div>
   );
 }
