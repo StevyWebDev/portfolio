69e19276177bb9dc4e6de2d669003501508e9528
StevyWebDev <StevyWebDev@gmail.com>
2021-12-26 17:44:26 +0100

    me, formtaion and skills ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Me from './components/Me';
+import Formation from './components/Formation';
+import Skills from './components/Skills';
+import {useEffect, useState} from 'react';
+
 //import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-        <main>
-            <Home />
-        </main>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Header/>
+            <main>
+                <Home/>
+                <Me/>
+                <Formation />
+                <Skills />
+            </main>
+        </div>
+    );
 }
 
 export default App;
